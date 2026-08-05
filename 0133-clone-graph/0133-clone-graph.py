056ee4e762e632a22ebe642d070991f63f366ef7
Satvik Reddy <satvikreddymallavarapu@gmail.com>
2026-08-05 23:25:36 +0530

    Time: 47 ms (74.12%), Space: 19.9 MB (18.29%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,20 @@
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+
+            copy = Node(node.val)
+            oldToNew[node] = copy
+
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+
+            return copy
+
+        return dfs(node)
