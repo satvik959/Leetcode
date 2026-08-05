afcaab199735e0df84fe1040fb59f909de990a16
Satvik Reddy <satvikreddymallavarapu@gmail.com>
2026-08-05 23:27:49 +0530

    Time: 0 ms (100%), Space: 19.5 MB (20.11%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        memo = {}
+
+        def dfs(start):
+            if start in memo:
+                return memo[start]
+
+            if start == len(s):
+                return [""]
+
+            res = []
+
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+
+                if word in wordSet:
+                    for sub in dfs(end):
+                        if sub:
+                            res.append(word + " " + sub)
+                        else:
+                            res.append(word)
+
+            memo[start] = res
+            return res
+
+        return dfs(0)
